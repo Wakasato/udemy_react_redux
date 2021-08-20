1ab714e1afb2041a36c0f4deeec545233b063751
Wakasato <susitalo710@hotmail.co.jp>
2021-08-20 18:52:12 +0900

    make store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStpre } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers'
+import App from './components/App';
+
+const store = createStore(reducer)
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store= {store}>
+       <App />
+    </Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
